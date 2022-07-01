0508348586e8ca950fc84401605e3ba0b6990b75
KyleLee <ghlee4215@naver.com>
2022-07-01 12:47:49 +0900

    [STYLE] 메인 스타일 수정 #1

--- wowdiz-fe/src/components/header/MainHeader.js
+++ wowdiz-fe/src/components/header/MainHeader.js
@@ -9,28 +9,30 @@ import '../../style/header.css';
 const Header = () => {
     return (
         <div className='header_wrap'>
-            {/* 헤더 메뉴버튼 */}
-            <NavLink to='/'><img src={logo} className="logo_image" alt=""/></NavLink>
-            <ul className='header_menu'>  
-                <li><NavLink to='/funding' className="menu_li" >펀딩</NavLink></li>
-                <li><NavLink to='/event'className="menu_li">이벤트</NavLink></li>
-                <li><NavLink to='/support' className="menu_li">고객센터</NavLink></li>
-            </ul>
-             {/* 서치 폼  */}
-            <div className='serch_var'>
-            <form method="GET" action="/pundingproject/search" className='search_form'>
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
-            </svg>
-            <input type="search" placeholder="프로젝트를 찾으시나요?"  className='keyword' autocomplete="off"/>
-            </form>
+            <div className='header_full'>
             </div>
-            {/* 유저 로그인 회원가입 버튼 */}
-            <span className='user_var'>
-            <NavLink to='/login' className="user_var_li">로그인</NavLink>
-            <NavLink to='/register'className="user_var_li">회원가입</NavLink>
-            </span>
-
+            {/* 헤더 메뉴버튼 */}
+                <NavLink to='/'><img src={logo} className="logo_image" alt=""/></NavLink>
+                <ul className='header_menu'>  
+                    <li><NavLink to='/funding' className="menu_li" >펀딩</NavLink></li>
+                    <li><NavLink to='/event'className="menu_li">이벤트</NavLink></li>
+                    <li><NavLink to='/support' className="menu_li">고객센터</NavLink></li>
+                </ul>
+                {/* 서치 폼  */}
+                <div className='serch_var'>
+                    <form method="GET" action="/pundingproject/search" className='search_form'>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                        </svg>
+                        <input type="search" placeholder="프로젝트를 찾으시나요?"  className='keyword' autocomplete="off"/>
+                    </form>
+                </div>
+                {/* 유저 로그인 회원가입 버튼 */}
+                <span className='user_var'>
+                    <NavLink to='/login' className="user_var_li">로그인</NavLink>
+                    <NavLink to='/register'className="user_var_li">회원가입</NavLink>
+                </span>
+           
         </div>
     );
 };
