70f8e69422eccf5e9a4dc7999daeed138d91cf55
LeeKiMin <1993km@naver.com>
2022-07-01 11:49:20 +0900

    [DESIGN] 로그인화면 프론트화면

--- wowdiz-fe/src/pages/user/login/Login.js
+++ wowdiz-fe/src/pages/user/login/Login.js
@@ -1,9 +1,37 @@
 import React from 'react';
-
+import "../../../style/login.css";
 const Login = () => {
     return (
         <div>
-            <h1>로그인 페이지입니다.</h1>
+            <div class="v302_74">
+                <div class="v302_75">
+                    <div class="v302_104">
+
+                    </div>
+                    </div>
+                    <div class="v302_77">
+                        <div class="v302_78"></div>
+                        <div class="v302_79"></div>
+                        <div class="v302_103"></div>
+                        <span class="v302_80">Login</span>
+                        <div class="v302_105"></div>
+                        <div class="v302_81"></div>
+                        <span class="v302_109">Login</span>
+                        <div class="v302_82"></div>
+                        </div>
+                        <div class="v302_88">
+                            <div class="v302_89"></div>
+                            </div>
+                            <div class="v302_90">
+                                <div class="v302_91"></div>
+                                </div>
+                                <div class="v302_92"><img src='../../../assets/logo/LOGO.png' alt=''></img></div>
+                                <span class="v302_97">Username</span>
+                                <span class="v302_98">Password</span>
+                                <span class="v302_99">아이디 찾기</span>
+                                <span class="v302_100">비밀번호 찾기</span>
+                                <span class="v302_101">회원가입</span>
+                                </div>
         </div>
     );
 };
