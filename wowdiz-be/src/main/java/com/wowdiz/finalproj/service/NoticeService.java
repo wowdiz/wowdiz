15ada5176369c8a9623033798ffaa071958e180e
LeeKiMin <1993km@naver.com>
2022-07-22 15:29:02 +0900

    [FEAT] 공지사항 레이아웃 수정 및 기능 구현

---
+++ wowdiz-be/src/main/java/com/wowdiz/finalproj/service/NoticeService.java
@@ -0,0 +1,10 @@
+package com.wowdiz.finalproj.service;
+
+import java.util.List;
+
+import com.wowdiz.finalproj.dto.NoticeDto;
+
+public interface NoticeService {
+
+	public List<NoticeDto> noticeList();
+}
--- wowdiz-fe/src/components/support/SupportBoard.js
+++ wowdiz-fe/src/components/support/SupportBoard.js
@@ -1,17 +1,22 @@
 import React from "react";
 import "../../style/notice.css";
-const SupportBoard = (props) => {
+const SupportBoard = ({data}) => {
+  console.log(data);
   return (
     <div className="notice_main">
       <ul className="notice_wrap">
         <li className="notice_container">
           <div className="notice_info">
-            <div className="notice_important"><b>중요(select)</b></div>
-            <div className="notice_thum"></div>
-            <div className="notice_title">title</div>
+            <div className="notice_important"><b>{(
+              data.important ==="Y"
+                  ? <p>중요 · BEST</p>
+                  : null
+                )}</b></div>
+            <div className="notice_thum">{data.notice_thumbnail}</div>
+            <div className="notice_title" Link to="#">{data.notice_title}</div>
             <br/>
-            <span className="notice_admin">wowdiz</span>
-            <span className="notice_date">2022.06.30</span>
+            <span className="notice_admin">{data.admin_id}</span>
+            <span className="notice_date">{data.write_date}</span>
           </div>
         </li>
       </ul>
